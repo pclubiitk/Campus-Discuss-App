052f0c55ff34b7999b813d91e4aaa959af7d0bc7
Kartikeya Gupta <guptakar20@gmail.com>
2020-07-26 19:32:28 +0530

    Remove login import

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import StackNavigator from "./ui/screens/login/RootStack";
 
 export default function App() {
   return (
