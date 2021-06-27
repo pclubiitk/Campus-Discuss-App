62654706d374f8327a06643c692b8373a87c312e
Abhinav Vishwakarma <80336377+AlphaFlagship@users.noreply.github.com>
2021-06-27 12:34:34 +0530

    Update BackButton.tsx

--- ui/Comments/BackButton.tsx
+++ ui/Comments/BackButton.tsx
@@ -1,24 +1,66 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {TouchableOpacity, Image, Text} from 'react-native';
-import {ListItem} from 'react-native-elements';
+import {TouchableOpacity, Image, Text, View, Appearance} from 'react-native';
 
 type Props = {
   onBack: () => void;
 };
-
+const colorScheme = Appearance.getColorScheme();
+let BackButtonContainer;
 const BackButton = (props: Props) => {
+  if (colorScheme === 'dark') {
+    BackButtonContainer = (
+      <TouchableOpacity onPress={props.onBack} activeOpacity={0.5}>
+        <View style={{flexDirection: 'row'}}>
+          <Image
+            source={require('../../assets/backButtonDark.png')}
+            style={{width: 35, height: 35, translateX: 5}}
+          />
+          <Text
+            style={{
+              justifyContent: 'center',
+              alignSelf: 'center',
+              translateX: 10,
+            }}>
+            Back
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  } else {
+    BackButtonContainer = (
+      <TouchableOpacity onPress={props.onBack} activeOpacity={0.5}>
+        <View style={{flexDirection: 'row'}}>
+          <Image
+            source={require('../../assets/backButtonLight.png')}
+            style={{width: 35, height: 35, translateX: 5}}
+          />
+          <Text
+            style={{
+              justifyContent: 'center',
+              alignSelf: 'center',
+              translateX: 10,
+            }}>
+            Back
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
   return (
-    <TouchableOpacity onPress={props.onBack} activeOpacity={0.5}>
-      <ListItem>
-        <Image
-          source={{
-            uri: 'E:/Coding/Visual_Studio_Code/CampusApp/assets/backButton.png',
-          }}
-          style={{resizeMode: 'cover', width: 40, height: 40}}
-        />
-        <title>back</title>
-      </ListItem>
-    </TouchableOpacity>
+    <View>
+      <View style={{flexDirection:'row'}}>
+      <Text style={{flex:1}}>{BackButtonContainer}</Text>
+      <Text style={{flex:2,textAlign:'center',textAlignVertical:'center',fontWeight:'bold'}}>Comments</Text>
+      <Text style={{flex:1,textAlign:'center'}} />
+      </View>
+      <View style={{flexDirection: 'row', alignSelf: 'center',alignContent:'center'}}>
+        <View style={{flex: 0.485, height: 1, backgroundColor: '#33BEF0'}} />
+        <View style={{flex: 0.485, height: 1, backgroundColor: '#33BEF0'}} />
+      </View>
+    </View>
+
   );
 };
 
